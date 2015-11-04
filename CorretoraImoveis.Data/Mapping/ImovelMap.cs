dcd4e490ef140993451cfaab3a222b349dd0b212
Varcal <cleber.varcal@varcalsys.com.br>
2015-11-04 11:59:11 -0200

    Merge dev_varcal

--- CorretoraImoveis.Data/Mapping/ImovelMap.cs
+++ CorretoraImoveis.Data/Mapping/ImovelMap.cs
@@ -11,61 +11,61 @@ namespace CorretoraImoveis.Data.Mapping
             this.HasKey(t => t.Id);
 
             // Properties
-            this.Property(t => t.Title)
+            Property(t => t.Title)
                 .IsRequired()
                 .HasMaxLength(500);
 
-            this.Property(t => t.Description)
+            Property(t => t.Description)
                 .IsRequired()
                 .HasMaxLength(500);
 
-            this.Property(t => t.Type)
+            Property(t => t.Type)
                 .IsRequired()
                 .HasMaxLength(50);
 
-            this.Property(t => t.Address)
+           Property(t => t.Address)
                 .IsRequired()
                 .HasMaxLength(500);
 
-            this.Property(t => t.Lat)
+            Property(t => t.Lat)
                 .IsRequired()
                 .HasMaxLength(50);
 
-            this.Property(t => t.Lng)
+            Property(t => t.Lng)
                 .IsRequired()
                 .HasMaxLength(50);
 
-            this.Property(t => t.Image)
+           Property(t => t.Image)
                 .IsRequired()
                 .HasMaxLength(200);
 
             // Table & Column Mappings
-            this.ToTable("Imovel");
-            this.Property(t => t.Id).HasColumnName("Id");
-            this.Property(t => t.Title).HasColumnName("Title");
-            this.Property(t => t.Description).HasColumnName("Description");
-            this.Property(t => t.Type).HasColumnName("Type");
-            this.Property(t => t.Price).HasColumnName("Price");
-            this.Property(t => t.Address).HasColumnName("Address");
-            this.Property(t => t.Bathrooms).HasColumnName("Bathrooms");
-            this.Property(t => t.Bedrooms).HasColumnName("Bedrooms");
-            this.Property(t => t.Area).HasColumnName("Area");
-            this.Property(t => t.Lat).HasColumnName("Lat");
-            this.Property(t => t.Lng).HasColumnName("Lng");
-            this.Property(t => t.DataRecord).HasColumnName("DataRecord");
-            this.Property(t => t.Garage).HasColumnName("Garage");
-            this.Property(t => t.SecuritySystem).HasColumnName("SecuritySystem");
-            this.Property(t => t.AirConditioning).HasColumnName("AirConditioning");
-            this.Property(t => t.Balcony).HasColumnName("Balcony");
-            this.Property(t => t.OutdoorPool).HasColumnName("OutdoorPool");
-            this.Property(t => t.Internet).HasColumnName("Internet");
-            this.Property(t => t.Heating).HasColumnName("Heating");
-            this.Property(t => t.TvCable).HasColumnName("TvCable");
-            this.Property(t => t.Garden).HasColumnName("Garden");
-            this.Property(t => t.Telephone).HasColumnName("Telephone");
-            this.Property(t => t.FirePlace).HasColumnName("FirePlace");
-            this.Property(t => t.Active).HasColumnName("Active");
-            this.Property(t => t.Image).HasColumnName("Image");
+            ToTable("Imovel");
+            Property(t => t.Id).HasColumnName("Id");
+            Property(t => t.Title).HasColumnName("Title");
+            Property(t => t.Description).HasColumnName("Description");
+            Property(t => t.Type).HasColumnName("Type");
+            Property(t => t.Price).HasColumnName("Price");
+            Property(t => t.Address).HasColumnName("Address");
+            Property(t => t.Bathrooms).HasColumnName("Bathrooms");
+            Property(t => t.Bedrooms).HasColumnName("Bedrooms");
+            Property(t => t.Area).HasColumnName("Area");
+            Property(t => t.Lat).HasColumnName("Lat");
+            Property(t => t.Lng).HasColumnName("Lng");
+            Property(t => t.DataRecord).HasColumnName("DataRecord");
+            Property(t => t.Garage).HasColumnName("Garage");
+            Property(t => t.SecuritySystem).HasColumnName("SecuritySystem");
+            Property(t => t.AirConditioning).HasColumnName("AirConditioning");
+            Property(t => t.Balcony).HasColumnName("Balcony");
+            Property(t => t.OutdoorPool).HasColumnName("OutdoorPool");
+            Property(t => t.Internet).HasColumnName("Internet");
+            Property(t => t.Heating).HasColumnName("Heating");
+            Property(t => t.TvCable).HasColumnName("TvCable");
+            Property(t => t.Garden).HasColumnName("Garden");
+            Property(t => t.Telephone).HasColumnName("Telephone");
+            Property(t => t.FirePlace).HasColumnName("FirePlace");
+            Property(t => t.Active).HasColumnName("Active");
+            Property(t => t.Image).HasColumnName("Image");
         }
     }
 }
